8beb4bd1a420fdc655bcc117bd583504bc56eefe
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-04-10 15:22:01 -0300

    Update androidlercidade.php

--- androidlercidade.php
+++ androidlercidade.php
@@ -4,28 +4,27 @@ $port        = "port = 5432";
 $dbname      = "dbname = de04qoln4k3dbd";
 $credentials = "user = qzijxcrgetwdyg password=c57cdc821172e503b8d71aebbb96cb5fc7d78adeb40f694720a128251e687c5b";
 $db = pg_connect( "$host $port $dbname $credentials"  );
+
+$idcidade = $_GET['id'];
+
 $sql =<<<EOF
-   SELECT * from urban.cidade order by idcidade;
+   SELECT * from urban.hotspot where idcidade = $idcidade order by idhotspot;
 EOF;
 $ret = pg_query($db, $sql);
 if(!$ret) {
      echo pg_last_error($db);
  exit;
 }
- $response["cidades"] = array();
+ $response["hotspot"] = array();
 while($row = pg_fetch_row($ret)) {
-   $cidade = array();
-   $cidade["id"] = $row[0];
-   $cidade["nome"] = $row[1];
-   $cidade["imagem"] = $row[2];
-   $cidade["latitude"] = $row[3];
-   $cidade["longitude"] = $row[4];
-   array_push($response["cidades"], $cidade);
+   $hotspot = array();
+   $hotspot["id"] = $row[0];
+   $hotspot["nome"] = $row[1];
+   $hotspot["imagem"] = $row[2];
+   $hotspot["latitude"] = $row[3];
+   $hotspot["longitude"] = $row[4];
+   array_push($response["hotspot"], $hotspot);
 }
 echo json_encode($response);
 pg_close($db);
-
-
-
-
- ?>
+?>
