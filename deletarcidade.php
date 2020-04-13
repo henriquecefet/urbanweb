de0b1d2cbffa24f3982cfd0fa383edf3dd4a5e7a
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-04-13 15:07:54 -0300

    Add files via upload

--- deletarcidade.php
+++ deletarcidade.php
@@ -4,11 +4,11 @@ include("conexao.php");
 <?php
   $idcidade = $_GET['id'];
    $sql =<<<EOF
-      DELETE from urban.cidade where idcidade = $$idcidade;
+      DELETE from urban.cidade where idcidade = $idcidade;
 EOF;
    $ret = pg_query($db, $sql);
    $sql2 =<<<EOF
-      DELETE from urban.hotspot where idcidade = $$idcidade;
+      DELETE from urban.hotspot where idcidade = $idcidade;
 EOF;
  $ret2 = pg_query($db, $sql);
    if(!$ret2) {
