1b8f0bbb0272ea7022a338414ec1c89de55ab877
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-04-16 18:10:33 -0300

    Add files via upload

--- deletarcomentario.php
+++ deletarcomentario.php
@@ -2,7 +2,7 @@
 include("conexao.php");
 ?>
 <?php
-  $idhcomentario = $_GET['id'];
+  $idcomentario = $_GET['id'];
    $sql =<<<EOF
       DELETE from urban.comentario where idhcomentario = $idcomentario;
 EOF;
