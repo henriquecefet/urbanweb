f8fc4efbda21157f524edc18ca9969de8ab65ec3
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-04-27 11:32:48 -0300

    Add files via upload

--- max.php
+++ max.php
@@ -19,6 +19,26 @@ EOF;
     break;
   }
   return $maxIX;
+}
+function pegarId($tabela, $coluna, $id)
+  $host        = "host = ec2-52-23-14-156.compute-1.amazonaws.com";
+  $port        = "port = 5432";
+  $dbname      = "dbname = de04qoln4k3dbd";
+  $credentials = "user = qzijxcrgetwdyg password=c57cdc821172e503b8d71aebbb96cb5fc7d78adeb40f694720a128251e687c5b";
+  $db = pg_connect( "$host $port $dbname $credentials"  );
+  $sql =<<<EOF
+     SELECT $coluna from $tabela where $nomeID = $id;
+EOF;
+  $ret = pg_query($db, $sql);
+  if(!$ret) {
+       echo pg_last_error($db);
+   exit;
+  }
+  while($row = pg_fetch_row($ret)) {
+    $colunarequerida = $row[0];
+    break;
+  }
+  return $colunarequerida ;
 
 }
  ?>
