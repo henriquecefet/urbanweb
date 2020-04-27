7264dffaca1be0cf05d1b0861f24ac4085301f0f
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-04-27 11:41:01 -0300

    Add files via upload

--- pegarcoluna.php
+++ pegarcoluna.php
@@ -1,13 +1,12 @@
 <?php
-function maxId($tabela, $nomeID){
-  $maxIX = 0;
+function pegarId($tabela, $coluna, $nomeID, $id)
   $host        = "host = ec2-52-23-14-156.compute-1.amazonaws.com";
   $port        = "port = 5432";
   $dbname      = "dbname = de04qoln4k3dbd";
   $credentials = "user = qzijxcrgetwdyg password=c57cdc821172e503b8d71aebbb96cb5fc7d78adeb40f694720a128251e687c5b";
   $db = pg_connect( "$host $port $dbname $credentials"  );
   $sql =<<<EOF
-     SELECT MAX($nomeID) from $tabela;
+     SELECT $coluna from $tabela where $nomeID = $id;
 EOF;
   $ret = pg_query($db, $sql);
   if(!$ret) {
@@ -15,9 +14,10 @@ EOF;
    exit;
   }
   while($row = pg_fetch_row($ret)) {
-    $maxIX = $row[0];
+    $colunarequerida = $row[0];
     break;
   }
-  return $maxIX;
+  return $colunarequerida ;
+
 }
- ?>
+?>
