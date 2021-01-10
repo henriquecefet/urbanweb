0c51fa24d6f724e04f8bb8df40ba5f001a76a7ed
henriquecefet <rodriguesoareshenrique@gmail.com>
2021-01-10 15:26:44 -0300

    Add files via upload

---
+++ apilervariascidades.php
@@ -0,0 +1,31 @@
+<?php
+include("conexao.php");
+?>
+<?php
+$sql =<<<EOF
+   SELECT * from urban.cidade order by idcidade;
+EOF;
+$ret = pg_query($db, $sql);
+if(!$ret) {
+     echo pg_last_error($db);
+ exit;
+}
+ $response["cidades"] = array();
+while($row = pg_fetch_row($ret)) {
+   $cidade = array();
+   $cidade["id"] = $row[0];
+   $cidade["nome"] = $row[1];
+   $cidade["imagem"] = $row[2];
+   $cidade["latitude"] = $row[3];
+   $cidade["longitude"] = $row[4];
+   $cidade["estado"] = $row[5];
+   $cidade["pais"] = $row[6];
+   array_push($response["cidades"], $cidade);
+}
+echo json_encode($response);
+pg_close($db);
+
+
+
+
+ ?>
