90e077783696980f3170767044fe6668b2438815
henriquecefet <rodriguesoareshenrique@gmail.com>
2021-08-11 16:52:41 -0300

    Add files via upload

---
+++ angrybirds/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1628711348,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/fundo-sheet0.png",
+		"images/chao-sheet0.png",
+		"images/madeira-sheet0.png",
+		"images/porco-sheet0.png",
+		"images/porco-sheet1.png",
+		"images/red-sheet0.png",
+		"images/catapulta-sheet0.png",
+		"images/rastro-sheet0.png",
+		"images/estilinquedireita-sheet0.png",
+		"images/estilinqueesquerda-sheet0.png",
+		"media/angry birds theme song.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
