922b79173ad97afd686e41484bb89c6edc216783
henriquecefet <rodriguesoareshenrique@gmail.com>
2021-12-06 21:09:07 -0300

    Add files via upload

---
+++ Pong/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1638835203,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bola-sheet0.png",
+		"images/fundo.png",
+		"images/lancador-sheet0.png",
+		"images/parede-sheet0.png",
+		"images/leftgoal-sheet0.png",
+		"media/pong.ogg",
+		"media/main theme (8-bit nes mix).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
